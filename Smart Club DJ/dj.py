ceb2d3d6e93b56572ceffff7f821a5df09065b6d
vk93 <VarunKumar2016@u.northwestern.edu>
2016-05-27 13:44:31 -0500

    updated code

--- Smart Club DJ/dj.py
+++ Smart Club DJ/dj.py
@@ -1,28 +1,29 @@
 import pyglet
 import time
-player = pyglet.media.Player()
-beep = pyglet.resource.media('beep-01a.wav')
-player.queue(beep)
-player.play()
-edm = pyglet.resource.media('Kav_Verhouzer_LarryKoek_-_People_ft.wav')
-house = pyglet.resource.media('04 Da Funk.wav')
-trap = pyglet.resource.media('01 Know Me.wav')
-rap = pyglet.resource.media('Waves.wav')
-pop = pyglet.resource.media('18 I Really Like You (Liam Keegan Remix Radio Edit).wav')
-player.queue(house)
-player.queue(trap)
-player.queue(rap)
-player.queue(pop)
-player.volume = 1.0
-while 1:
-    try:
-        player.play()
-        time.sleep(15)
-        player.next_source()
-    except KeyboardInterrupt:
-        print "interrupt"
-        break
 
+def play(type):
+    edm = pyglet.resource.media('Kav_Verhouzer_LarryKoek_-_People_ft.wav')
+    house = pyglet.resource.media('04 Da Funk.wav')
+    trap = pyglet.resource.media('01 Know Me.wav')
+    rap = pyglet.resource.media('Waves.wav')
+    pop = pyglet.resource.media('18 I Really Like You (Liam Keegan Remix Radio Edit).wav')
+    player = pyglet.media.Player()
+    if type == "same":
+        player.queue(house)
+    else:
+        player.queue(pop)
+    player.queue(edm)
+    player.volume = 1.0
+    while 1:
+        try:
+            player.play()
+            time.sleep(10)
+            player.pause()
+            return play("same")
+            player.next_source()
+        except KeyboardInterrupt:
+            print "interrupt"
+            break
 
-player.pause()
-pyglet.app.run()
+play("a")
+sys.exit()
